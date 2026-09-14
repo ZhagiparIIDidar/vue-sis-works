a158aa25a6881c8f440de9ebaf20bb8d40f4d886
didar <zhagipardidarit@gmail.com>
2026-09-14 12:59:16 +0500

    init proj + readme

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 8000, // или любой другой свободный
+    host: true  // заодно слушать на 0.0.0.0/IPv4 тоже
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
