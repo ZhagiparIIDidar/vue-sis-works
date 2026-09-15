cf71cc25d316e21c6ffcc14f423d6b276e3f2236
didar <zhagipardidarit@gmail.com>
2026-09-15 18:31:45 +0500

    first commit

---
+++ src/views/Products.vue
@@ -0,0 +1,17 @@
+<script setup>
+const products = defineProps({products})
+</script>
+
+<template>
+  <h1>Products</h1>
+
+  <ol>
+    <li v-for="prod in products" :key="prod.id">
+      {{ prod.id }} - {{ prod.name }}
+    </li>
+  </ol>
+</template>
+
+<style scoped>
+
+</style>
