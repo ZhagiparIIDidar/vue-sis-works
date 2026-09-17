edfbfb48b9c890568acb826d4075b33976a6e424
didar <zhagipardidarit@gmail.com>
2026-09-18 01:29:06 +0500

    refactor files and dirs

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './assets/main.css'
-
-
-/* ==========================================================
-   APP BOOTSTRAP
-   ========================================================== */
-createApp(App).mount('#app')
